d96ee6686ad4a98134612b4f6cf684e55c769136
1cfgs <1cfgs@A6PC03>
2021-03-03 11:19:24 +0100

    paso 3 y 4

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -11,6 +11,50 @@
 public class Calculadora {
     private int operador1;
     private int operador2;
-    private int operador3;
+    private int operacion;
+
+    /**
+     * @return the operador1
+     */
+    public int getOperador1() {
+        return operador1;
+    }
+
+    /**
+     * @param operador1 the operador1 to set
+     */
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    /**
+     * @return the operador2
+     */
+    public int getOperador2() {
+        return operador2;
+    }
+
+    /**
+     * @param operador2 the operador2 to set
+     */
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    /**
+     * @return the operador3
+     */
+    public int getOperador3() {
+        return operacion;
+    }
+
+    /**
+     * @param operador3 the operador3 to set
+     */
+    public void setOperador3(int operador3) {
+        this.operacion = operador3;
+    }
+    
+    
     
 }
